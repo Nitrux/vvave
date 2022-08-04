309d1024df47086938189ad933d09b432551f203
Uri Herrera <hello@uriherrera.com>
2022-08-03 19:07:38 -0500

    ffs

--- scripts/build-deb.sh
+++ scripts/build-deb.sh
@@ -23,7 +23,7 @@ DEBIAN_FRONTEND=noninteractive apt -qq update
 
 DEBIAN_FRONTEND=noninteractive apt -qq -yy install --no-install-recommends \
 	mauikit-git \
-	mauikit-accounts \
+	mauikit-accounts-git \
 	mauikit-filebrowsing-git \
 	taglib
 
